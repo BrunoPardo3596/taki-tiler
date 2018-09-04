e105ba555e7ace80e210129393087ac19bac73b7
Taqtile <taqtile@Coopers.local>
2018-09-04 20:07:37 -0300

    Pagina inicial de login

--- taki-tiler/src/App.js
+++ taki-tiler/src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <div>
+          <header className="App-header">
+            
+            <h1 className="App-title">Welcome to Taki-Tiler</h1>
+          </header>
+          {/* <p className="App-intro">
+            To begin this new experience, sign in!
+          </p> */}
+          <fieldset>
+              <legend>Sign in</legend>
+              <form>
+                <div>
+                    <label htmlFor="userName">Username:</label>
+                    <input type="email" pattern=".+@taqtile.com"
+                          id="username" name="username" placeholder="taki-tiler@taqtile.com"
+                          required title="Please enter a valid email"/>
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password"
+                          minLength="4" required title="Password must contain at least 4 characters"
+                          placeholder="4 characters minimum" />
+                </div>
+
+                <input type="submit" value="Sign in" />
+              </form>
+
+          </fieldset>
+
+          
+        </div>
       </div>
     );
   }
