73363219bc8fda5c1782e1074af93566875e95bc
Taqtile <taqtile@Coopers.local>
2018-09-05 18:01:40 -0300

    Escopo do router, ainda com erro

--- taki-tiler/src/App.js
+++ taki-tiler/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
+  constructor(props){
+    super(props)
 
-  state = {
-    email: '',
-    password: '',
-    emailValid: false,
-    passwordValid: false,
-    appPage: 'login'
+    this.state = {
+      email: '',
+      password: '',
+      emailValid: false,
+      passwordValid: false,
+      appPage: 'login'
+    }
   }
 
+  
   onSubmitHandler = () => {
 
     if(this.state.emailValid === true && this.state.passwordValid === true){
@@ -96,6 +100,54 @@ class App extends Component {
         {appPageX}
       </div>
     );
+
+    // const login = () => (
+    //   <div>
+    //     <header className="App-header">
+    //       <h1 className="App-title">Welcome to Taki-Tiler</h1>
+    //     </header>
+    //     <fieldset>
+    //         <legend>Sign in</legend>
+    //         <div>
+    //           <label htmlFor="userName">Username:</label>
+    //           <input type="email" 
+    //                 required 
+    //                 pattern=".+@taqtile.com"
+    //                 id="username" 
+    //                 name="username" 
+    //                 placeholder="taki-tiler@taqtile.com"
+    //                 title="Please enter a valid email"
+    //                 onBlur={this.emailChangedHandler}/>
+    //         </div>
+    //         <div>
+    //           <label htmlFor="password">Password:</label>
+    //           <input type="password" 
+    //                 id="password" 
+    //                 name="password"
+    //                 minLength="4"  
+    //                 required title="Password must contain at least 4 characters"
+    //                 placeholder="4 characters minimum" 
+    //                 onBlur={this.passwordChangedHandler}/>
+    //         </div>
+    //         <button value="Sign in" onClick={this.onSubmitHandler}>Sign In</button>
+    //     </fieldset>
+    //   </div>
+    // )
+
+    // const nextPage = () => (
+    //   <div>
+    //      <h1>OUTRA PAGINA</h1>
+    //   </div>
+    // )
+
+    // return (
+    //   <Router>
+    //     <div className="App">
+    //       <Route exact path="/" component={login}/>
+    //       <Route path="/nextPage" component={nextPage}/>
+    //     </div>
+    //   </Router>
+    // );
   }
 }
 
