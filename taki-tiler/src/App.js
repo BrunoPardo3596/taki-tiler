bb550af4e160fcd997d6c3c9ba8c79cef8c4a600
Taqtile <taqtile@Coopers.local>
2018-09-05 14:13:31 -0300

    Melhorias nos codigos

--- taki-tiler/src/App.js
+++ taki-tiler/src/App.js
@@ -3,41 +3,92 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    email: '',
+    password: '',
+    emailValid: false,
+    passwordValid: false,
+    appPage: 'login'
+  }
+
+  onSubmitHandler = () => {
+
+    if(this.state.emailValid === true && this.state.passwordValid === true){
+      document.body.style.cursor='wait';
+      this.setState({appPage: "appPageX"})
+    }
+    else console.log(this.state);
+    
+  }
+
+  emailChangedHandler = (event) => {
+    const email = event.target.value;
+    this.setState({email: email});
+
+    const validateFormat = email.includes("@taqtile.com");
+    this.setState({emailValid: validateFormat});
+  }
+
+  passwordChangedHandler = (event) => {
+    const password = event.target.value;
+    this.setState({password: password});
+    
+    const validateFormat = password.length >= 4;
+    this.setState({passwordValid: validateFormat});
+  }
+
   render() {
-    return (
-      <div className="App">
+
+    let login = null;
+    let appPageX = null;
+
+    if(this.state.appPage === "login"){
+      login = (
+
         <div>
           <header className="App-header">
             
             <h1 className="App-title">Welcome to Taki-Tiler</h1>
           </header>
-          {/* <p className="App-intro">
-            To begin this new experience, sign in!
-          </p> */}
           <fieldset>
               <legend>Sign in</legend>
-              <form>
-                <div>
-                    <label htmlFor="userName">Username:</label>
-                    <input type="email" pattern=".+@taqtile.com"
-                          id="username" name="username" placeholder="taki-tiler@taqtile.com"
-                          required title="Please enter a valid email"/>
-                </div>
+              
+              <div>
+                  <label htmlFor="userName">Username:</label>
+                  <input type="email" required pattern=".+@taqtile.com"
+                        id="username" name="username" placeholder="taki-tiler@taqtile.com"
+                        title="Please enter a valid email"
+                        onBlur={(event) => this.emailChangedHandler(event)}
+                        />
+              </div>
 
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password"
-                          minLength="4" required title="Password must contain at least 4 characters"
-                          placeholder="4 characters minimum" />
-                </div>
+              <div>
+                  <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" name="password"
+                        minLength="4"  required title="Password must contain at least 4 characters"
+                        placeholder="4 characters minimum" 
+                        onBlur={(event) => this.passwordChangedHandler(event)}/>
+              </div>
 
-                <input type="submit" value="Sign in" />
-              </form>
-
+              <button value="Sign in" onClick={this.onSubmitHandler}>Sign In</button>
           </fieldset>
-
-          
         </div>
+
+      );
+    }
+
+    else{
+      appPageX = (
+        <div>
+          <h1>OUTRA PAGINA</h1>
+        </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        {login}
+        {appPageX}
       </div>
     );
   }
