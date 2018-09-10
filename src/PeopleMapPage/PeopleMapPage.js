46f14c0f91e0f176151b3f1f83e3067671e1bf96
Taqtile <taqtile@Coopers.local>
2018-09-10 17:27:27 -0300

    reset para separar branches de listagem e de detalhes de usuario

--- src/PeopleMapPage/PeopleMapPage.js
+++ src/PeopleMapPage/PeopleMapPage.js
@@ -15,34 +15,39 @@ class PeopleMapPage extends Component {
         allPeople: []
     }
   }
-
   
   handlePageClick = (data) => {
     let selected = data.selected;
-    let offset = Math.ceil(selected * this.state.perPage);
+    //let offset = Math.ceil(selected * this.state.perPage);
 
-    this.setState({offset: offset}, () => {
+    this.setState({offset: selected}, () => {
       this.loadCommentsFromServer();
     });
   };
 
   loadCommentsFromServer() {
-    let data = this.state.allPeople.slice(this.state.offset, this.state.perPage + this.state.offset);
-    this.setState({people: data});
+    //let data = this.state.allPeople.slice(this.state.offset, this.state.perPage + this.state.offset);
+    axios.get("https://tq-template-server-sample.herokuapp.com/users?pagination={\"page\":" + this.state.offset +", \"window\": 3}",
+    {headers: {Authorization: localStorage.getItem("token")}})
+    .then(response => {
+      this.setState({people: response.data.data});
+    })
+    
   }
 
   componentDidMount(){
-    axios.get("https://tq-template-server-sample.herokuapp.com/users?pagination={\"page\": 0 , \"window\": 10}",
+    axios.get("https://tq-template-server-sample.herokuapp.com/users?pagination={\"page\": 0 , \"window\": 3}",
         {headers: {Authorization: localStorage.getItem("token")}})
         .then(response => {
-            this.setState({allPeople: response.data.data}),
-            this.setState({people: response.data.data.slice(0, this.state.perPage)})
+          this.setState({allPeople: response.data.data}),
+          this.setState({people: response.data.data})
         });
   }
 
   render(){
 
-    let pageCount = this.state.allPeople.length/this.state.perPage ;
+    // let pageCount = this.state.allPeople.length/this.state.perPage ;
+    let pageCount = 15;
 
     return(
         <div className="PersonList">
@@ -51,7 +56,7 @@ class PeopleMapPage extends Component {
             </div>
             <h2>Taki Tiler user list:</h2>
             <div className="PeopleBox">
-                <People people={this.state.people}></People>
+                <People people={this.state.people} prop={this.props}></People>
                 <ReactPaginate 
                     previousLabel={"previous"}
                     nextLabel={"next"}
